138efd172c51f923b84a8857083820fd1a470c8a
Jason Vanhalwyn <jvanhalwyn@icloud.com>
2018-02-23 23:38:11 +0100

    try to figure out why it's not working in prod

--- db/migrate/20180222102146_create_cocktails.rb
+++ db/migrate/20180222102146_create_cocktails.rb
@@ -2,7 +2,7 @@ class CreateCocktails < ActiveRecord::Migration[5.1]
   def change
     create_table :cocktails do |t|
       t.string :name
-      t.string :photo_url
+      t.string :photo
 
       t.timestamps
     end
